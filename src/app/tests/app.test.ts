0f16e21dc37782435067e196bef644d244acdefc
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2022-09-11 17:21:39 -0300

    rename app/app.ts to app/index.ts

--- src/app/tests/app.test.ts
+++ src/app/tests/app.test.ts
@@ -1,4 +1,4 @@
-import {App, AppCommand} from "../app";
+import {App, AppCommand} from "../index";
 import WebSocket from "ws";
 import {
     createOpenSocket,
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import {App} from "./app/app";
+import {App} from "./app";
 import WebSocket from "ws";
 import {FileSystemBasedReminderRepository} from "./app/repositories";
 
